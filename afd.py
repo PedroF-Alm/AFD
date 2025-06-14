3840d1320455ac7bbb0728403ba8f0d7a7dbfe07
PedroF-Alm <pedrofernandesacc@gmail.com>
2025-06-14 20:08:17 -0300

    Reformulação para usar OO e arquivo de configuração json

--- afd.py
+++ afd.py
@@ -1,89 +1,28 @@
-def getMachine():
-    machine = {'(': 'LParenteses', 
-               ')': 'RParenteses', 
-               '{': 'LChave',  
-               '}': 'RChave',
-               '[': 'LColchete',
-               ']': 'RColchete',
-               '=': {'=': 'EQ', '$': 'Atribuicao'},
-               '>': {'=': 'GEQ', '$': 'GT'},
-               '<': {'=': 'LEQ', '$': 'LT'},
-               '!': {'$':'NEG'},
-               ',': 'Virgula',
-               ';': 'PVirgula',
-               'i': {'f': {' ': 'IF'}, 'n': {'t': {' ': 'INTDEF'}}},               
-               '+': 'SUM',
-               '-': 'SUB',
-               '*': 'MULT',
-               '/': 'DIV',
-               '%': 'RESTO',
-               'f': {'l': {'o': {'a': {'t': {' ': 'FLOATDEF'}}}}},
-               '&': {'&': 'AND'},
-               '|': {'|': 'OR'},                                
-               '.': {},
-               'c': {'h': {'a': {'r': {' ': 'CHAR_TYPE'}}}},       
-               'b': {'o': {'o': {'l': {' ': 'BOOL_TYPE'}}}},       
-               'r': {'e': {'t': {'u': {'r': {'n': {' ': 'RETURN'}}}}}},       
-               'v': {'a': {'r': {' ': lambda : Exception}}},       
-               }
-    
-    for i in range(0, 10):
-        machine[str(i)] = {'$': 'NUM_INT', '.': {}}
-        machine['.'][str(i)] = {'$': 'NUM_FLOAT'}
+class AFD:
 
-    aux = [chr(j) for j in range(ord('a'), ord('z') + 1)]
-    aux.extend([chr(j) for j in range(ord('A'), ord('Z') + 1)])
+    def __init__(self, estado_inicial, estados_finais, transicoes):
+        self.estado_inicial = estado_inicial
+        self.estados_finais = estados_finais
+        self.transicoes = transicoes
 
-    for i in aux:
-        machine['v']['a']['r'][i] = {'$': 'VAR'}
-
-    for i in aux:
-        for j in aux:
-            machine['v']['a']['r'][i][j] = machine['v']['a']['r'][i]
-
-    for i in range(0, 10):
-        for j in range(0, 10):
-            machine[str(i)][str(j)] = machine[str(i)]            
-            machine[str(i)]['.'][str(j)] = {'$': 'NUM_FLOAT'}              
-            machine['.'][str(i)][str(j)] = machine['.'][str(i)]     
-            for k in range(0, 10):
-                machine[str(i)]['.'][str(j)][str(k)] = machine[str(k)]['.']  
-  
-    return machine
-
-def processFile(file):     
-    machine = getMachine()
-    line = file.readline()
-    line_number = 1
-    output = ''
-    while line != '':
-        machine = getMachine()
-        i = 0
-        while i < len(line):
-            symbol = line[i]
+    def processarLinha(self, linha):
+        estado = self.estado_inicial
+        resultado = ''
+        for i in linha:
             try:
-                if symbol in machine.keys() and (type(machine[symbol]) == str):
-                    print(machine[symbol], end=' ')
-                    output += machine[symbol]
-                    machine = getMachine()
-                elif symbol in machine.keys():
-                    machine = machine[symbol] 
-                elif '$' in machine.keys():
-                    print(machine['$'], end=' ')
-                    output += machine['$']
-                    machine = getMachine()
-                    i -= 1
-                else:
-                    machine = getMachine()
-                    i += 1
-                    raise Exception
-                i += 1
-            except:     
-                if (symbol != '\n' and symbol != ' '):
-                    print(f"\nFalhou em {line_number}:{i}")
-                    return None   
-        line = file.readline()
-        line_number += 1
-        print()    
-        output += '\n'
-    return output
+                estado = self.transicoes[estado][i]
+            except:
+                return f'Símbolo \'{i}\' não suportado pelo AFD no estado ({estado})'
+            if estado in self.estados_finais:
+                resultado += estado + ' '
+                estado = self.estado_inicial
+        return resultado
+    
+    def processarArquivo(self, nome_arquivo_entrada):
+        with open(nome_arquivo_entrada, 'r') as arquivo_entrada:
+            linha = arquivo_entrada.readline()
+            saida = ''
+            while linha != '':
+                saida += self.processarLinha(linha) + '\n'
+                linha = arquivo_entrada.readline()
+            return saida[0:len(saida)-1]
--- main.py
+++ main.py
@@ -1,21 +1,22 @@
 import sys
-from afd import processFile
+import json
+from afd import AFD
 
-def printFile(file):
-    line = file.readline()
-    while line != "":
-        print(line, end="")
-        line = file.readline()
-    
 if len(sys.argv) < 2:
     print("No input file.")
     exit(1)
 
-file = open(sys.argv[1])
-output = processFile(file)
-file.close()
+with open('./config.json', 'r') as config:
+    config = json.loads(config.read())
+    estado_inicial = config['estado_inicial']
+    estados_finais = config['estados_finais']
+    transicoes     = config['transicoes']
+    
+    afd = AFD(estado_inicial, estados_finais, transicoes)
+    saida = afd.processarArquivo(sys.argv[1])
+    print(saida)
 
-if len(sys.argv) > 2 and output != None:
-    output_file = open(sys.argv[2], 'w')
-    output_file.write(output)
-    output_file.close()
+    if len(sys.argv) > 2:
+        arquivo_saida = open(sys.argv[2], 'w')
+        arquivo_saida.write(saida)
+        arquivo_saida.close()
